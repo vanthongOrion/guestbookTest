cb3eeb769e8d6ff2afe3b48754b60de22455f08c
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-04-04 21:24:19 +0900

    Modify Controller/ConferenceController.php CommentRepository.php show.html.twig

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -38,11 +38,16 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/conference/{id}", name="conference")
      */
-    public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository)
+    public function show(Request $request, Environment $twig, Conference $conference, CommentRepository $commentRepository)
     {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference,$offset);
+        
         return new Response($twig->render('conference/show.html.twig',[
             'conference' => $conference,
-            "comments" => $commentRepository->findBy(['conference' => $conference],['createAt' => 'DESC'])
-            ,]));
+            'comments'   => $paginator,
+            'previous'   => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next'       => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            ]));
     }
 }
