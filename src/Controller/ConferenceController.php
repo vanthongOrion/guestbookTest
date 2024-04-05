58a6455e718e61b303743be80c23db843c1bca60
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-04-05 17:16:37 +0900

    change from id to slug

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -43,7 +43,7 @@ class ConferenceController extends AbstractController
     }
 
     /**
-     * @Route("/conference/{id}", name="conference")
+     * @Route("/conference/{slug}", name="conference")
      */
     public function show(Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository)
     {
