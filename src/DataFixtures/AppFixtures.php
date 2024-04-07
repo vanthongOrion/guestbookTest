5d3f93b569ef7818fac3d28870f41a623a7e3002
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-04-07 21:38:50 +0900

    create AppFixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,50 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Admin;
+use App\Entity\Comment;
+use App\Entity\Conference;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private PasswordHasherFactoryInterface $passwordHasherFactory,
+    )
+    {
+
+    }
+
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
+        $hanoi = new Conference();
+        $hanoi->setCity('Hanoi');
+        $hanoi->setYear('2019');
+        $hanoi->setIsInternational(true);
+        $manager->persist($hanoi);
+
+        $paris = new Conference();
+        $paris->setCity('Paris');
+        $paris->setYear('2020');
+        $paris->setIsInternational(false);
+        $manager->persist($paris);
+
+        $comment1 = new Comment();
+        $comment1->setConference($hanoi);
+        $comment1->setAuthor('Thong');
+        $comment1->setEmail('vanthong@ar-system.co.jp');
+        $comment1->setText('This is thong \'s comment');
+        $manager->persist($comment1);
+
+        $admin = new Admin();
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setUsername('admin');
+        $admin->setPassword($this->passwordHasherFactory->getPasswordHasher(Admin::class)->hash('admin'));
+        $manager->persist($admin);
 
         $manager->flush();
     }
