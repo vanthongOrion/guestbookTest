45b9ec9f29c81397a6331bf9ed269523ddce5dc6
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-04-08 14:23:38 +0900

    create case test testCommentSubmission

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -23,9 +23,23 @@ class ConferenceControllerTest extends WebTestCase
 
         $client->clickLink(('View'));
 
-        $this->assertPageTitleContains('Amstertdam');
+        $this->assertPageTitleContains('Hanoi');
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h2','Amsterdam 2019');
+        $this->assertSelectorTextContains('h2','Hanoi 2023');
         $this->assertSelectorExists('div:contains("There are 1 comments")');
+    }
+
+    public function testCommentSubmission()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/conference/hanoi-2023');
+        $client->submitForm('Submit' ,['comment_form[author]' => 'Fabien', 
+                                       'comment_form[text]' => 'Some feedback from an automated functional test',
+                                       'comment_form[email]' => 'me@automat.ed',
+                                       'comment_form[photo]' => dirname(__DIR__,2) .'/public/images/under-construction.gif',
+                                       ]);
+        $this->assertResponseRedirects();
+        $client->followRedirects();
+        $this->assertSelectorExists('div:contains("There are 2 comments")');
     }
 }
