76f0db960c4ce81dc3e9610cd70228c9530d5a12
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-04-08 14:54:10 +0900

    Entity\Comment.php: property 'state' make default value

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -39,8 +39,8 @@ class Comment
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $photofilename = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $state = null;
+    #[ORM\Column(length: 255, options: ['default' => 'submitted'])]
+    private ?string $state = 'submitted';
 
     public function getId(): ?int
     {
