e5fc5da47e89bdbffdf296fe7a6fee1e7e277582
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-04-08 14:02:10 +0900

    create testConferencePage function

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -13,4 +13,19 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2','Give your feedback');
     }
+
+    public function testConferencePage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('Get', '/');
+
+        $this->assertCount(2, $crawler->filter('h4'));
+
+        $client->clickLink(('View'));
+
+        $this->assertPageTitleContains('Amstertdam');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2','Amsterdam 2019');
+        $this->assertSelectorExists('div:contains("There are 1 comments")');
+    }
 }
